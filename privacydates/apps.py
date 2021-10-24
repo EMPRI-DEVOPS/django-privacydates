2b7a90c48b55af9182e8277955cdb0f80b78f6d8
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-10-25 00:00:00 +0200

    Set per-app auto primary key type

--- privacydates/apps.py
+++ privacydates/apps.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_delete, post_save
 
 class PrivacydatesConfig(AppConfig):
     name = 'privacydates'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from .signals import delete_datetime_of_deleted_parent, event_creator_signal_receiver
