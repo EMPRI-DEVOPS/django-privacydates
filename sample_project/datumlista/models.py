dce3f37ad3ffe55085ab928de80f9e86158f9286
Christian Burkert <burkert@informatik.uni-hamburg.de>
2021-11-08 00:00:00 +0100

    More sample project cleanup

--- sample_project/datumlista/models.py
+++ sample_project/datumlista/models.py
@@ -5,11 +5,15 @@ from privacydates import fields
 from privacydates.mixins import VanishingDateMixIn
 
 
-# Basic Event with multiple timestamps representing the same time with the different types of privacydates date fields.
 class Event(models.Model, VanishingDateMixIn):
-    base_date = models.DateTimeField(null=True, blank=True, default=timezone.now)
-    rough_date = fields.RoughDateField(default=timezone.now, null=True, blank=True, reduction_value=30)
+    """Basic Event with multiple timestamps representing the same time with the
+    different types of privacydates date fields."""
+    base_date = models.DateTimeField(
+        null=True, blank=True, default=timezone.now)
+    rough_date = fields.RoughDateField(
+        default=timezone.now, null=True, blank=True, reduction_value=30)
     vanishing_date = fields.VanishingDateField()
     vanishing_ordering_date = fields.VanishingDateField()
     ordering_date = fields.OrderingDateField(null=True, blank=True)
-    ordering_similarity_date = fields.OrderingDateField(null=True, blank=True, similarity_distance=2)
+    ordering_similarity_date = fields.OrderingDateField(
+        null=True, blank=True, similarity_distance=2)
